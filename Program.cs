9b3db9e3b3b6b204206e7179ce3cfb82ebb631ff
v-joazh <v-joazh@CPC-vjoaz-E15LL>
2025-07-09 20:35:38 +0800

    12334

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace RootProject
+{
+    public class RootClass
+    {
+        public string GetMessage()
+        {
+            return "Hello from RootProject!";
+        }
+    }
+}
